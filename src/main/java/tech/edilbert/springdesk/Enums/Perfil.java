7a2298d318da140c9fb3e8954efa32e84898bdc9
Edilbert Nunes <edilbert.nunes@gmail.com>
2023-02-26 15:14:05 -0300

    Criando as Classes do Projeto Parte 1

---
+++ src/main/java/tech/edilbert/springdesk/Enums/Perfil.java
@@ -0,0 +1,23 @@
+package tech.edilbert.springdesk.Enums;
+
+public enum Perfil {
+
+    ADMIN("Administrador"),
+    TECNICO("Tecnico"),
+    CLIENTE("Cliente");
+
+    private String perfil;
+  
+    private Perfil(String perfil) {
+        this.perfil = perfil;
+    }
+
+    public String getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(String perfil) {
+        this.perfil = perfil;
+    }
+
+}
